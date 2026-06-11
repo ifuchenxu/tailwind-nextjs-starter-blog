5b6e1d493b280049d8af2c63274636242f9ea67a
ifuchenxu <ifuchenxu@gmail.com>
2026-06-11 19:44:19 +0800

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: '首页' },
   { href: '/blog', title: '随笔' },
   { href: '/tags', title: '标签分类' },
-  #{ href: '/projects', title: 'Projects' },
+  //{ href: '/projects', title: 'Projects' },
   { href: '/about', title: '关于道长' },
 ]
 
