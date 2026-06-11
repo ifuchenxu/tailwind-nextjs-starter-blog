cc0c6bef4517def4eb96a0a755a2ca91a78b5f35
ifuchenxu <ifuchenxu@gmail.com>
2026-06-11 19:52:23 +0800

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -77,7 +77,7 @@ export default function Home({ posts }) {
             className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label="All posts"
           >
-            All Posts &rarr;
+            查看全部 &rarr;
           </Link>
         </div>
       )}
