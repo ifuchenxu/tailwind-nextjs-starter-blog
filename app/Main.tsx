7fead47e6b5415a64f4bafa8f98c651cd0b3e23b
ifuchenxu <ifuchenxu@gmail.com>
2026-06-11 21:46:30 +0800

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -65,7 +65,7 @@ export default function Home({ posts }) {
                     </div>
                   </div>
                 </article>
-              </td>
+              </li>
             )
           })}
         </ul>
